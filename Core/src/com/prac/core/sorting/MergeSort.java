da207f7daec667a952574b5f18cf43b398f1c06c
krsnastudy <krsna.study@gmail.com>
2020-05-10 11:20:35 +0530

    1

--- Core/src/com/prac/core/sorting/MergeSort.java
+++ Core/src/com/prac/core/sorting/MergeSort.java
@@ -3,42 +3,29 @@ package com.prac.core.sorting;
 public class MergeSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		int array[] = {10, 3, 2, 5, 8, 4, 3, 1, 2, 9, 7, 8};
 		
 		System.out.println("User entered Array: ");
 		MergeSort.printArray(array);
 		
 		long start = System.nanoTime();
-		
 		MergeSort.mergeSort(array, 0, array.length-1);
-		
 		long end = System.nanoTime();
 		
-		System.out.println("\n\nTime to execute this algo: " + (end-start));
-		
+		System.out.println("\nTime to execute this algo: " + (end-start));
 		System.out.println("\nAfter sorting: ");
 		MergeSort.printArray(array);
-		
 	}
 	
-	
 	public static void mergeSort(int[] Array, int left, int right) {
-		
 		if (right > left) {
-			
 			int m = (left + right) / 2;
-			
 			mergeSort(Array, left, m);
-			
 			mergeSort(Array, m + 1, right);
-			
 			merge(Array, left, m, right);
 		}
 	}//end of method
 
-	
 	static void merge(int[] A, int left, int middle, int right) {
 		
 		int [] leftTmpArray = new int[middle-left+2];  //Create tmp arrays
@@ -53,7 +40,6 @@ public class MergeSort {
 		leftTmpArray[middle-left+1]= Integer.MAX_VALUE; //Merge values and insert into Array 'A'
 		rightTmpArray[right-middle] = Integer.MAX_VALUE;
 		
-		
 		int i = 0, j = 0;
 		for (int k = left; k <= right; k++) {
 			if (leftTmpArray[i] < rightTmpArray[j]) {
@@ -66,13 +52,9 @@ public class MergeSort {
 		}
 	}//end of method
 	
-	
 	public static void printArray(int []array) {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i]+"  ");
 		}
 	}//end of method
-
-	
-
 }
