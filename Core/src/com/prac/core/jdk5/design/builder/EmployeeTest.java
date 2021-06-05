accbe56195812868a041d64b0e2f553316a42331
DESKTOP-5O7OPDH\User <krsna.study@gmail.com>
2021-06-05 16:13:43 +0530

    Upadated

---
+++ Core/src/com/prac/core/jdk5/design/builder/EmployeeTest.java
@@ -0,0 +1,15 @@
+package com.prac.core.jdk5.design.builder;
+
+public class EmployeeTest {
+	public static void main(String[] ars) {
+		Employee.Builder emplBuilder = new Employee.Builder();
+
+		Employee employee = 
+					emplBuilder.setName("baeldung")
+							   .setAge(12)
+							   .setDepartment("Builder Pattern")
+							   .build();
+		
+		System.out.println(employee);
+	}
+}
