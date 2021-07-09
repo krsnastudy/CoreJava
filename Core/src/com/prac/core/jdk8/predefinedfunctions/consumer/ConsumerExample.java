a172a312de8b16a911c51ab7e769919d90d2df9b
DESKTOP-5O7OPDH\User <krsna.study@gmail.com>
2021-07-09 13:59:48 +0530

    Update

--- Core/src/com/prac/core/jdk8/predefinedfunctions/consumer/ConsumerExample.java
+++ Core/src/com/prac/core/jdk8/predefinedfunctions/consumer/ConsumerExample.java
@@ -1,13 +1,51 @@
 package com.prac.core.jdk8.predefinedfunctions.consumer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import com.prac.core.jdk8.stream.employee.data.Employee;
+import com.prac.core.jdk8.stream.employee.data.PopulateEmpData;
 
 public class ConsumerExample {
 
 	public static void main(String[] args) {
 //		Consumer<String> c = x->System.out.println(x);
 		Consumer<String> c = System.out::println;
-		c.accept("RK");
-	}
+//		c.accept("RK");
 
+		List<Employee> empData = new ArrayList<Employee>();
+		int noOfRecords = 10; // How many records you want
+		empData = PopulateEmpData.populateEmpData(noOfRecords);
+
+		Consumer<List<Employee>> consumer = emp -> emp.stream().distinct()
+				.forEach(x -> System.out.println(x.toString()));
+
+		List<Employee> filterData = empData.stream().distinct().filter(e -> e.geteSal() > 80000)
+				.collect(Collectors.toList());
+
+		System.out.println("\nConsumer accept() RawData");
+		consumer.accept(empData);
+
+		System.out.println("\nConsumer accept() FilteredData");
+		consumer.accept(filterData);
+
+		Consumer<List<Employee>> chained1 = chain -> chain.stream().distinct()
+//				.filter(s -> s.getDepartment().equals("IT"))
+				.filter(s -> s.geteSal() > 90000)
+				.forEach(x -> System.out.println(x.toString()));
+
+		Consumer<List<Employee>> chained2 = chain -> chain.stream().distinct()
+//				.filter(s -> s.getDepartment().equals("Admin"))
+				.filter(y->y.getfName().contains("s"))
+				.forEach(x -> System.out.println(x.toString()));
+
+		Consumer<List<Employee>> chainedCosumer = chained1.andThen(chained2);
+
+		System.out.println("\nConsumer accept() ChainingData");
+		chained1.accept(empData);
+		chainedCosumer.accept(empData);
+
+	}
 }
