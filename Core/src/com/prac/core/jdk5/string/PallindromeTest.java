8cb2dc2da9dcc0c4f1bde93284bb36e92670445c
krsnastudy <krsna.study@gmail.com>
2022-01-19 09:14:16 +0530

    add

--- Core/src/com/prac/core/jdk5/string/PallindromeTest.java
+++ Core/src/com/prac/core/jdk5/string/PallindromeTest.java
@@ -12,8 +12,39 @@ public class PallindromeTest {
 		set.add("Hare Rama");
 		set.add("Rama Rama");
 		set.add("Hare Hare");
-
 		System.out.println(set);
+
+		int n = 45354;
+		String s = "MADAM";
+		System.out.println(n + " Is " + (nPallindrome(n) ? "Pallindrome" : "Not Pallindrome"));
+		System.out.println(s + " Is " + (sPallindrome(s) ? "Pallindrome" : "Not Pallindrome"));
+	}
+
+	public static boolean nPallindrome(int n) {
+		boolean isPallindrome = false;
+		int rem, sum = 0, temp = n;
+
+		while (n > 0) {
+			rem = n % 10;
+			sum = rem + (sum * 10);
+			n = n / 10;
+		}
+
+		isPallindrome = (temp == sum) ? true : false;
+
+		return isPallindrome;
+	}
+
+	public static boolean sPallindrome(String str) {
+		String reverse = "";
+
+		for (int i = (str.length() - 1); i >= 0; i--) {
+			reverse = reverse + str.charAt(i);
+//			System.out.println(str.charAt(i)+" -- "+reverse);
+		}
+
+		return reverse.equals(str);
+
 	}
 
 }
