f8f092a8fc665ff53d6549cbb45664a914807f96
krsnastudy <krsna.study@gmail.com>
2022-03-08 09:53:27 +0530

    Updated

--- Core/src/com/prac/core/jdk8/string/CharsCount.java
+++ Core/src/com/prac/core/jdk8/string/CharsCount.java
@@ -5,13 +5,14 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class CharsCount {
 
 	public static void main(String[] args) {
 		String str = new String("Radha Krishna");
-
+		Consumer<Map.Entry<Character, Integer>> consume = i->System.out.print(i+" ");
 		Map<Character, Integer> count = new HashMap<>();
 
 		str = str.replaceAll(" ", "").toLowerCase();
@@ -45,13 +46,15 @@ public class CharsCount {
 		                k -> Character.valueOf((char) k.intValue()),
 		                v -> 1,         // 1 occurence
 		                Integer::sum)); // counting
-		System.out.println("Frequencies:\n" + frequencies);
+		System.out.println("Frequencies:\n");
 		
+		//Print Alphabetically
+//		frequencies.entrySet().stream().sorted((i1, i2)->i1.getKey().compareTo(i2.getKey())).forEach(i->System.out.print(i+" "));
+		frequencies.entrySet().stream().sorted((i1, i2)->i1.getKey().compareTo(i2.getKey())).forEach(i->consume.accept(i));
 		
+		System.out.println("\n \nCollections.frequency Example");
 		List<Integer> list = Arrays.asList(5, 3, 4, 1, 3, 7, 2, 9, 9, 4);
 //		list.stream().filter(i -> Collections.frequency(list, i) > 1).collect(Collectors.toSet()).forEach(System.out::println);;
 		list.stream().filter(i->Collections.frequency(list, i)>1).distinct().forEach(System.out::println);
 	}
-
-
 }
--- Core/src/com/prac/core/problem/persisistant/StringPractice2.java
+++ Core/src/com/prac/core/problem/persisistant/StringPractice2.java
@@ -1,5 +1,7 @@
 package com.prac.core.problem.persisistant;
 
+import java.util.Arrays;
+
 public class StringPractice2 {
 
 	public static void main(String[] args) {
@@ -10,5 +12,23 @@ public class StringPractice2 {
 		for (int i = (str.length() - 1); i >= 0; i--) {
 			System.out.print(str.charAt(i));
 		}
+
+		/* Practice */
+		System.out.println("\n");
+		System.out.println('A' + 'B');
+		System.out.println('A' + "B");
+		System.out.println('A' + 'B' + "C");
+		System.out.println("A" + "B" + 'A' + 10);
+		System.out.println(20 + 1.34f + "A" + "B");
+		char[] strArr = { 'i', 'n', 'c', 'l', 'u', 'd', 'e', 'h', 'e', 'l', 'p' };
+		System.out.println(strArr.toString());
+		System.out.println(Arrays.toString(strArr));
+
 	}
 }
+
+/*
+ * Explanation: If you try to concatenate any different types of data like
+ * integer, character, float with string value, the result will be a string. So
+ * ‘A’ will be concatenated with “AB” and answer will be “ABA”.
+ */
