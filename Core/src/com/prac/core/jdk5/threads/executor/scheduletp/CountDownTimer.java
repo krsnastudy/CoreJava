0dace645a79e1a2111f73aac1d5fb231dd47ec79
krsnastudy <krsna.study@gmail.com>
2022-04-09 11:55:43 +0530

    Uploaded

---
+++ Core/src/com/prac/core/jdk5/threads/executor/scheduletp/CountDownTimer.java
@@ -0,0 +1,29 @@
+package com.prac.core.jdk5.threads.executor.scheduletp;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class CountDownTimer {
+
+	public static void main(String[] args) {
+		final ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+
+		Runnable r = new Runnable() {
+			int count = 20;
+
+			@Override
+			public void run() {
+				System.out.println(count);
+				count--;
+
+				if (count < 0) {
+					System.out.println("Time Over!");
+					service.shutdown();
+				}
+			}
+		};
+
+		service.scheduleAtFixedRate(r, 0, 1, TimeUnit.SECONDS);
+	}
+}
