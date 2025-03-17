fd18a0e565ad721f571c63d15917900e60b7819c
RadhaKrishna Ch V <krsna.study@gmail.com>
2025-03-17 23:20:01 +0530

    Updated

---
+++ src/com/prac/core/jdk5/mem/WeakHashMapExample.java
@@ -0,0 +1,27 @@
+package com.prac.core.jdk5.mem;
+
+import java.util.WeakHashMap;
+
+public class WeakHashMapExample {
+    public static void main(String[] args) {
+        WeakHashMap<Object, String> weakMap = new WeakHashMap<>();
+        Object key1 = new Object(); // Strong reference
+        Object key2 = new Object(); // Strong reference
+
+        weakMap.put(key1, "Java");
+        weakMap.put(key2, "Spring");
+
+        System.out.println("Before GC: " + weakMap);
+
+        // Remove strong reference
+        key1 = null;
+
+        // Force Garbage Collection
+        System.gc();
+
+        // Wait for GC to finish
+        try { Thread.sleep(1000); } catch (InterruptedException e) {}
+
+        System.out.println("After GC: " + weakMap);
+    }
+}
