0684c0f77099e5109ca561e229dda235b7f3b720
RadhaKrishna Ch V <krsna.study@gmail.com>
2025-04-09 14:45:07 +0530

    Update

--- src/com/prac/core/jdks/jdk8/classmethods/MethodOverLoadNULL.java
+++ src/com/prac/core/jdks/jdk8/classmethods/MethodOverLoadNULL.java
@@ -1,21 +1,34 @@
 package com.prac.core.jdks.jdk8.classmethods;
 
+import com.prac.core.jdks.jdk8.stream.employee.data.Employee;
+
 public class MethodOverLoadNULL {
-    // Overloaded methods
-    public void fun(Integer i) {
-        System.out.println("fun(Integer ) ");
-    }
-
-    public void fun(String name) {
-        System.out.println("fun(String ) ");
-    }
-
     // Driver code
     public static void main(String[] args) {
         MethodOverLoadNULL mv = new MethodOverLoadNULL();
-
-        // This line causes error
-//        mv.fun(null); // Ambiguous method call. Both
         mv.fun((Integer) null);
+        mv.MethodObject(5);
+    }
+
+    // Overloaded methods
+    public void fun(Integer i) {
+        System.out.println("fun(Integer) got called");
+    }
+
+    public void fun(String name) {
+        System.out.println("fun(String) got called ");
+    }
+
+    public void MethodObject(Object object){
+        System.out.println("MethodObject: "+object.toString());
     }
 }
+
+/*
+* mv.fun(null);
+* Ambiguous method call. Both
+     fun (Integer) in MethodOverLoadNULL and
+     fun(String)in MethodOverLoadNULL match
+*
+* mv.fun((Object) null); //Cannot resolve method 'fun(Object)'
+* */
--- src/com/prac/core/jdks/jdk8/threads/threadlocal/ThreadLocalExample.java
+++ src/com/prac/core/jdks/jdk8/threads/threadlocal/ThreadLocalExample.java
@@ -1,8 +1,11 @@
 package com.prac.core.jdks.jdk8.threads.threadlocal;
 
+import lombok.SneakyThrows;
+
 public class ThreadLocalExample {
     private static ThreadLocal<Integer> threadLocal = ThreadLocal.withInitial(() -> 0);
 
+    @SneakyThrows
     public static void main(String[] args) {
         Runnable task = () -> {
             threadLocal.set((int) (Math.random() * 100)); // each thread gets its own value
@@ -14,5 +17,29 @@ public class ThreadLocalExample {
 
         t1.start();
         t2.start();
+
+        t1.join();
+        t2.join();
+/*        int thread_count = 50;
+        Thread[] threads = new Thread[thread_count];
+        IntStream.range(1, thread_count)
+                .forEach(i->{
+                    int threadId = i;
+                    threads[i] = new Thread(() -> {
+                        System.out.println("Thread " + threadId + " running on " + Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    });
+                    threads[i].start();
+                });*/
+        // Join threads using stream
+/*        for (Thread t : threads) {
+            t.join();
+        }*/
+
+        System.out.println("All threads finished.");
     }
 }
