f0ac6dc2d6b421bad417be85cc27035361960a3f
RadhaKrishna Ch V <krsna.study@gmail.com>
2025-04-21 09:45:16 +0530

    update

--- src/com/prac/core/jdks/jdk8/stream/employee/EmployeeParallelStream.java
+++ src/com/prac/core/jdks/jdk8/stream/employee/EmployeeParallelStream.java
@@ -1,5 +1,8 @@
 package com.prac.core.jdks.jdk8.stream.employee;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.Period;
 import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.function.Function;
@@ -15,7 +18,7 @@ public class EmployeeParallelStream {
     @SuppressWarnings({"static-access", "unused"})
     public static void main(String[] args) {
         Supplier<Long> currTimeInMillSec = () -> Calendar.getInstance().getTimeInMillis();
-        int noOfRecords = 100000; // How many records you want
+        int noOfRecords = 1000000; // How many records you want
         List<Employee> emp = new ArrayList<Employee>();
         PopulateEmpData exec = new PopulateEmpData();
         emp = exec.populateEmpData(noOfRecords);
@@ -36,19 +39,30 @@ public class EmployeeParallelStream {
         Map<DepartmentCode, Long> dept = empDistinct.stream()
                 .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
 
-        System.out.println("Deptwise Count :: "+dept);
+        String strCollect = empDistinct.parallelStream().collect(Collectors.groupingBy(Employee::getDepartment))
+                .entrySet().stream().map(m -> m.getKey()+" Department Count :: " + m.getValue().size())
+                .collect(Collectors.joining("\n"));
+        System.out.println(strCollect+"\n");
+        System.out.println(noOfRecords+" Employees, Department Wise Split Count :: "+dept);
 
+        Instant now = Instant.now();
         long start; long end;
+
         start = currTimeInMillSec.get();
             Map<DepartmentCode, List<Employee>> deptStream =
                     empDistinct.stream()
                             .collect(Collectors.groupingBy(Employee::getDepartment));
-        end = currTimeInMillSec.get();
 
-        System.out.println("For " + noOfRecords + " records   Normal Stream Took: " + (end - start) + " MilliSeconds");
+        end = currTimeInMillSec.get();
+        Duration between = Duration.between(now, Instant.now());
+
+        System.out.println("\nFor " + noOfRecords + " records   Normal Stream Took: " + (end - start) + " MilliSeconds");
+        System.out.println("For " + noOfRecords + " records   Normal Stream Took: " +  between.toMinutes() + " Mins, "+ between.getSeconds()+ " Sec, "+
+                between.toMillis()+ " ms");
 
 //start = Calendar.getInstance().getTimeInMillis();
         start = ZonedDateTime.now().toInstant().toEpochMilli();
+        now = Instant.now();
 
         Map<DepartmentCode, List<Employee>> deptParallelStream =
                 empDistinct.parallelStream()
@@ -56,9 +70,11 @@ public class EmployeeParallelStream {
 //end = Calendar.getInstance().getTimeInMillis();
 
         end = ZonedDateTime.now().toInstant().toEpochMilli();
+        between = Duration.between(now, Instant.now());
 
-        System.out.println("For " + noOfRecords + " records Parallel Stream Took: " + (end - start) + " MilliSeconds\n");
-
+        System.out.println("\nFor " + noOfRecords + " records Parallel Stream Took: " + (end - start) + " MilliSeconds");
+        System.out.println("For " + noOfRecords + " records Parallel Stream Took: " +  between.toMinutes() + " Mins, "+ between.getSeconds()+ " Sec, "+
+                between.toMillis()+ " ms");
         /*
          * dept1.forEach((x,y)->System.out.println(x+" Departement has " + //y
          * y.stream().count()+" Employees" ));
