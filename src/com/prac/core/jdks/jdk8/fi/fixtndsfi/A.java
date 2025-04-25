90af17a5acb7dfd42a613416982395e066464c35
RadhaKrishna Ch V <krsna.study@gmail.com>
2025-04-25 21:40:48 +0530

    update

---
+++ src/com/prac/core/jdks/jdk8/fi/fixtndsfi/A.java
@@ -0,0 +1,6 @@
+package com.prac.core.jdks.jdk8.fi.fixtndsfi;
+
+@FunctionalInterface
+interface A {
+    void methodA(); // Single abstract method
+}
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,7 +1,15 @@
 package test.java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test1 {
     public static void main(String[] args) {
-
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, null);
+        map.put(2, null);
+        map.put(null, 1);
+        map.put(null, 2);
+        System.out.println(map);
     }
 }
