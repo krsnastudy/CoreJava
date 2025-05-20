5653732e325b0ad7e93f098fc8026a0299e4aab3
RadhaKrishna <krsna.study@gmail.com>
2025-05-20 22:49:21 +0530

    Update

--- src/com/prac/core/problem/companies/HCL/apr2025/CharCount.java
+++ src/com/prac/core/problem/companies/HCL/apr2025/CharCount.java
@@ -1,6 +1,8 @@
 package com.prac.core.problem.companies.HCL.apr2025;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -13,5 +15,27 @@ public class CharCount {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
         System.out.println(collect);
+
+        Map<Character, Long> collect1 = str.chars().mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(collect1);
+
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : str.toCharArray()){
+            map.merge(c, 1, Integer::sum);
+        }
+        System.out.println(map);
+
+        Map<Character, Integer> collect2 = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o,n)->o, LinkedHashMap::new));
+
+        System.out.println("Sort By Key   :: "+collect2);
+
+        LinkedHashMap<Character, Integer> collect3 = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, n) -> o, LinkedHashMap::new));
+
+        System.out.println("Sort By Value :: "+collect3);
     }
 }
