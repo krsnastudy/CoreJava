c5aca179cbafb3aaa5bf421f5a9d30ada23b48b6
Krsna <krsna.study@gmail.com>
2026-07-25 16:42:24 +0530

    updations

--- src/com/prac/core/practice/stream/employee/EmployeeStreamPractice.java
+++ src/com/prac/core/practice/stream/employee/EmployeeStreamPractice.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 @SuppressWarnings("unused")
 public class EmployeeStreamPractice {
     @SuppressWarnings("static-access")
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         PopulateEmpData exec = new PopulateEmpData();
         int noOfRecords = new Random().nextInt(5, 999);
         int salRandom = new Random().nextInt(1, 9);
@@ -28,14 +28,14 @@ public class EmployeeStreamPractice {
         // Employee FirstName StartsWith K
         System.out.println("\nEmployee FirstName/LastName StartsWith K");
         empList.stream()
-                .filter(f-> (f.getFName().startsWith("K") || f.getLName().startsWith("K")))
+                .filter(f -> (f.getFName().startsWith("K") || f.getLName().startsWith("K")))
                 .forEach(System.out::println);
 
         //Employee Details Salary >50000
-        System.out.println("\nEmployees Salary>"+(salFilter/1000)+" Thousands");
+        System.out.println("\nEmployees Salary>" + (salFilter / 1000) + " Thousands");
         empList.stream()
-        		.sorted(Comparator.comparing(Employee::getESal))
-                .filter(f->f.getESal()>salFilter)
+                .sorted(Comparator.comparing(Employee::getESal))
+                .filter(f -> f.getESal() > salFilter)
                 .forEach(System.out::println);
 
         //Employee Details Sort By FirstName
@@ -49,7 +49,7 @@ public class EmployeeStreamPractice {
         System.out.println("\nEmployee Details Sort By FirstName, LastName");
         Comparator<Employee> nameComparator =
                 Comparator.comparing(Employee::getFName)
-                          .thenComparing(Employee::getLName);
+                        .thenComparing(Employee::getLName);
 
         empList.stream()
                 .sorted(nameComparator)
@@ -57,19 +57,19 @@ public class EmployeeStreamPractice {
 
         //Employee Details Sum Of Salary
         Float totSal =
-        empList.stream()
-                .map(m->m.getESal())
-                .reduce((float)0, (a, b)->a+b);
-        System.out.println("\nSum of All Employees Salary : "+totSal);
+                empList.stream()
+                        .map(m -> m.getESal())
+                        .reduce((float) 0, (a, b) -> a + b);
+        System.out.println("\nSum of All Employees Salary : " + totSal);
 
         //Employee Details Dept Wise Sort By Salary
         System.out.println("\nPrinting Employees Dept Wise Sorting By Salary");
         empList.stream()
                 .collect(Collectors.groupingBy(Employee::getDeptCode))
                 .entrySet().stream()
-                .forEach(e->{
-                    System.out.println("\n/*************************  Department "+e.getKey()+"  ********************/\n"+
-                        e.getValue().stream().sorted(Comparator.comparing(Employee::getESal)).toList()
+                .forEach(e -> {
+                    System.out.println("\n/*************************  Department " + e.getKey() + "  ********************/\n" +
+                            e.getValue().stream().sorted(Comparator.comparing(Employee::getESal)).toList()
                     );
                 });
 
@@ -77,9 +77,9 @@ public class EmployeeStreamPractice {
         System.out.println("\nSum of All Employees Salary Dept Wise: ");
         empList.stream()
                 .collect(Collectors.groupingBy(Employee::getDeptCode))
-                .forEach((k,v)->{
-                    System.out.println("Department "+k + " Salary Total : "+
-                        v.stream().map(m->m.getESal()).reduce((float)0, (a,b)->a+b)
+                .forEach((k, v) -> {
+                    System.out.println("Department " + k + " Salary Total : " +
+                            v.stream().map(m -> m.getESal()).reduce((float) 0, (a, b) -> a + b)
                     );
                 });
 
@@ -88,46 +88,46 @@ public class EmployeeStreamPractice {
         empList.stream()
                 .collect(Collectors.groupingBy(Employee::getDeptCode))
                 .entrySet().stream()
-                .forEach(e->{
-                    System.out.println("Department "+e.getKey()+" --> " +
-                        e.getValue().stream()
-                                .collect(Collectors.summarizingDouble(Employee::getESal))
+                .forEach(e -> {
+                    System.out.println("Department " + e.getKey() + " --> " +
+                                    e.getValue().stream()
+                                            .collect(Collectors.summarizingDouble(Employee::getESal))
 //                                .getMin()
 //                                .getMax()
-                                .getAverage()
+                                            .getAverage()
 //                                .getCount()
                     );
                 });
 
         // Highest Salaried Employee
         Employee highSal =
-        empList.stream().collect(Collectors.maxBy(Comparator.comparing(Employee::getESal))).get();
-        System.out.println("\nHighest Salary Paid Employee : "+highSal);
+                empList.stream().collect(Collectors.maxBy(Comparator.comparing(Employee::getESal))).get();
+        System.out.println("\nHighest Salary Paid Employee : " + highSal);
 
         // Lowest Salaried Employee
         Employee lowSal = empList.stream().collect(Collectors.minBy(Comparator.comparing(Employee::getESal))).get();
-        System.out.println("Lowest Salary Paid Employee : "+lowSal);
+        System.out.println("Lowest Salary Paid Employee : " + lowSal);
 
         // Indexed Base highest Salary
         int idx = 2;
         Employee idxSal =
-        empList.stream().sorted(Comparator.comparing(Employee::getESal)).toList().get(idx-1);
-        System.out.println(idx+" Highest Salary Paid Employee : "+idxSal);
+                empList.stream().sorted(Comparator.comparing(Employee::getESal)).toList().get(idx - 1);
+        System.out.println(idx + " Highest Salary Paid Employee : " + idxSal);
 
         // Department Wise Indexed Base highest Salary
-        int dIdx = new Random().nextInt(2, (noOfRecords/10));
-        String rankPrefix = dIdx<2 ? ("st") : ((dIdx>2 && dIdx<3)?"nd":(dIdx>2 && dIdx<4)?"rd":"th");
-        
-        System.out.println("\n"+dIdx+rankPrefix+" Highest Salary Department Wise");
-                empList.stream()
-                        .collect(Collectors.groupingBy(Employee::getDeptCode))
-                        .entrySet().stream()
-                        .filter(f->(dIdx-1)<f.getValue().size())
-                        .forEach(e->{
-                            System.out.println(e.getKey() +" Department "+dIdx+rankPrefix+" Highest Salary --> " +
-                                e.getValue().stream().sorted(Comparator.comparing(Employee::getESal)).toList().get(dIdx-1)
-                            );
-                        });
+        int dIdx = new Random().nextInt(2, (noOfRecords / 10));
+        String rankPrefix = dIdx < 2 ? ("st") : ((dIdx > 2 && dIdx < 3) ? "nd" : (dIdx > 2 && dIdx < 4) ? "rd" : "th");
+
+        System.out.println("\n" + dIdx + rankPrefix + " Highest Salary Department Wise");
+        empList.stream()
+                .collect(Collectors.groupingBy(Employee::getDeptCode))
+                .entrySet().stream()
+                .filter(f -> (dIdx - 1) < f.getValue().size())
+                .forEach(e -> {
+                    System.out.println(e.getKey() + " Department " + dIdx + rankPrefix + " Highest Salary --> " +
+                            e.getValue().stream().sorted(Comparator.comparing(Employee::getESal)).toList().get(dIdx - 1)
+                    );
+                });
 
         // Department Wise Count of Employees
         System.out.println("\nEmployees Total Count Department Wise");
@@ -140,9 +140,9 @@ public class EmployeeStreamPractice {
         System.out.println("\nEmployees Sorting By Sal, Dept");
         empList.stream()
                 .sorted(Comparator.comparing(Employee::getDeptCode).thenComparing(Employee::getESal))
-                .collect(Collectors.toMap(Employee::getENumber, Function.identity(), (a,b)->a, LinkedHashMap::new))
+                .collect(Collectors.toMap(Employee::getENumber, Function.identity(), (a, b) -> a, LinkedHashMap::new))
                 .entrySet().stream()
-                .forEach(e-> System.out.println("EmployeeId: "+e.getKey()+" ["+e.getValue().getFName()+" "+e.getValue().getLName()+"] --> "+e.getValue()))
+                .forEach(e -> System.out.println("EmployeeId: " + e.getKey() + " [" + e.getValue().getFName() + " " + e.getValue().getLName() + "] --> " + e.getValue()))
         ;
 
         // Fetch Employees By Gender Base
@@ -153,9 +153,9 @@ public class EmployeeStreamPractice {
 
         // Fetch Employees By Gender Base
         empList.stream()
-               .collect(Collectors.groupingBy(Employee::getGender))
-               .entrySet().stream().sorted(Map.Entry.comparingByKey())
-               .forEach(e-> System.out.println("\n"+e.getKey()+" Gender "+"\n"+e.getValue()));
+                .collect(Collectors.groupingBy(Employee::getGender))
+                .entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .forEach(e -> System.out.println("\n" + e.getKey() + " Gender " + "\n" + e.getValue()));
 
         // Fetch Employees of DOJ in Year 2022
 /*        int doj = 2022;
@@ -167,46 +167,45 @@ public class EmployeeStreamPractice {
         // Group Employees by DOJ
         empList.stream()
                 .collect(Collectors.groupingBy(yr -> CommonUtil.getYear(yr.getDateOfJoin())))
-                .forEach((x,y)-> System.out.println("\n/********** Year: "+x+"**********/\n"+y));
+                .forEach((x, y) -> System.out.println("\n/********** Year: " + x + "**********/\n" + y));
 
         System.out.println("\n/****** Employees Salary Summation By Yearwise *****/");
         empList.stream()
-                .collect(Collectors.groupingBy(g->CommonUtil.getYear(g.getDateOfJoin())))
+                .collect(Collectors.groupingBy(g -> CommonUtil.getYear(g.getDateOfJoin())))
                 .entrySet().stream().sorted(Map.Entry.comparingByKey())
-                .forEach(e-> System.out.println(e.getKey()+" Year Employees Count "+e.getValue().size()+", Salary Total: "+
+                .forEach(e -> System.out.println(e.getKey() + " Year Employees Count " + e.getValue().size() + ", Salary Total: " +
 //                        e.getValue().stream().collect(Collectors.summarizingDouble(Employee::getESal)).getSum()
-						BigDecimal.valueOf(e.getValue().stream().collect(Collectors.summarizingDouble(Employee::getESal)).getSum()).setScale(2, RoundingMode.HALF_UP)
-                        ));
+                                BigDecimal.valueOf(e.getValue().stream().collect(Collectors.summarizingDouble(Employee::getESal)).getSum()).setScale(2, RoundingMode.HALF_UP)
+                ));
 
         System.out.println("\n/****** Employees Salary Summation By Gender *****/");
         empList.stream()
                 .collect(Collectors.groupingBy(Employee::getGender))
                 .entrySet().stream()
-                .forEach(x-> System.out.println(x.getKey()+" Total Salary: "+
+                .forEach(x -> System.out.println(x.getKey() + " Total Salary: " +
 //                        x.getValue().stream().map(m->m.getESal()).reduce((float)0, (a,b)->a+b)
 //                        x.getValue().stream().collect(Collectors.summarizingDouble(Employee::getESal)).getSum()
-						BigDecimal.valueOf(x.getValue().stream().collect(Collectors.summarizingDouble(Employee::getESal)).getSum()).setScale(2, RoundingMode.HALF_UP)	
-                    )
+                                        BigDecimal.valueOf(x.getValue().stream().collect(Collectors.summarizingDouble(Employee::getESal)).getSum()).setScale(2, RoundingMode.HALF_UP)
+                        )
                 );
 
-        
-        
+
         /************** Total Count ****************/
-        System.out.println("\nTotal Records Processing: "+noOfRecords);
+        System.out.println("\nTotal Records Processing: " + noOfRecords);
 
         /************** Sort Employees by Dept, Gender, fName, lName ****************/
         System.out.println("\n/************** Sort Employees by Dept, Gender, fName, lName ****************/");
         Comparator<Employee> empComparator = Comparator.comparing(Employee::getGender).reversed()
-                        .thenComparing(Employee::getFName).thenComparing(Employee::getLName);
+                .thenComparing(Employee::getFName).thenComparing(Employee::getLName);
 
         empList.stream()
                 .collect(Collectors.groupingBy(Employee::getDeptCode))
                 .entrySet().stream()
                 .limit(10)
-                .forEach(e->{
-                    System.out.println("/************ "+e.getKey()+" ****************/");
+                .forEach(e -> {
+                    System.out.println("/************ " + e.getKey() + " ****************/");
                     System.out.println(e.getValue().stream().sorted(empComparator)
-                            .map(m->m.getGender()+" "+m.getENumber()+" "+m.getFName()+" "+m.getLName())
+                            .map(m -> m.getGender() + " " + m.getENumber() + " " + m.getFName() + " " + m.getLName())
                             .collect(Collectors.joining("\n")));
                 });
 
@@ -219,5 +218,19 @@ public class EmployeeStreamPractice {
                 .collect(Collectors.joining("\n"));
 
         System.out.println(empJoined);
+
+
+        /********** Print Sum of Sal dept wise *****/
+        System.out.println("/********** Print Sum of Sal dept wise *****/");
+        empList.stream()
+                .collect(Collectors.groupingBy(Employee::getDeptCode))
+                .entrySet().stream()
+                .forEach(e -> {
+                    System.out.println("Department :: " + e.getKey() + " Total Salary : " +
+                            e.getValue().stream()
+                                    .map(Employee::getESal)
+                                    .reduce((a, b) -> a + b).get().doubleValue()
+                    );
+                });
     }//psvm
 }//StreamPractice
